eb3f33c3476963b1d4ca28b3ce16863dbc2a6231
amarizzal <rizal.ammar91@yahoo.com>
2020-07-06 16:22:57 +0700

    Tambah File Tampilan Web D'omah

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          '100': '#ffb300',
+          '200': '#ffd267',
+          '300': '#DFB10F',
+          '400': '#FFE6AB'
+        },
+        gray: {
+          '100': '#c4c4c4',
+          '200': '#413E36'
+        },
+      }
+    },
+    fontFamily: {
+      'main': ['"lemon"'],
+      'sec': ['cocogoose']
+    },
+    boxShadow: {
+      '2xl': '1px 5px 10px 0px rgba(0, 0, 0, 0.2)'
+    }
+  },
+  variants: {},
+  plugins: [],
+}
